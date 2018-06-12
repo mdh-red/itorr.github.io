7b33e00cec22ce9579ae1f30b367e63b938787df
kuaixiang <529723671@qq.com>
2018-06-12 23:38:58 +0800

    test2

--- config.js
+++ config.js
@@ -1,13 +1,12 @@
 
 docute.init({
-    home: 'home.md',
     landing: 'landing.html',
     title: 'M Nav',
     twitter: 'MenhoodT',
     nav: {
         default: [
             {
-                title: '首页', path: '/'
+                title: '首页', path: '/home'
             },
             {
                 title: '资源', path: '/resources'
